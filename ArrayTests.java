1b2aba5b99c6947f9385f7520359d37e9e316f6d
alt012ucsd <alt012@ucsd.edu>
2023-04-19 18:02:07 -0700

    Fixed

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,24 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+  @Test
+  public void testReverseInPlace2(){
+    int[] input2 = { 9, 8, 7, 6};
+  ArrayExamples.reverseInPlace(input2);
+  assertArrayEquals(new int[]{ 6, 7, 8, 9}, input2);
+  }
+    
+
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversed2(){
+    int[] input2 = {5, 6, 7, 8};
+    assertArrayEquals(new int[] {8, 7, 6, 5}, ArrayExamples.reversed (input2));
   }
 }
